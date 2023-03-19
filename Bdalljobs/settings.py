8631403d0bbfb528487a523cd80dba7ae496f6bb
rayhun <rayhunkhan27@gmail.com>
2023-03-19 13:41:20 +0600

    update settings

--- Bdalljobs/settings.py
+++ Bdalljobs/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'taggit',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
 
     # Local App
     'Core'
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Bdalljobs.urls'
